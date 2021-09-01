66558903fb6f6d1576edcf6862ba979e3687e7b3
halil93ibrahim <halil93ibrahim@gmail.com>
2021-09-01 14:46:41 +0200

    publish float array for image

--- src/depth_ros_python/realsense_depth_completion.py
+++ src/depth_ros_python/realsense_depth_completion.py
@@ -2,6 +2,7 @@ import rospy
 import cv2
 from sensor_msgs.msg import Image as msg_Image
 from sensor_msgs.msg import CameraInfo
+from std_msgs.msg import Float32MultiArray
 from cv_bridge import CvBridge, CvBridgeError
 import sys
 import os
@@ -16,7 +17,8 @@ from ip_basic import vis_utils
 class ImageListener:
     def __init__(self, depth_image_topic, depth_info_topic):
 
-        self.pub =  rospy.Publisher('/processed_depth_images', msg_Image, queue_size=10)
+        self.pub_array_image = rospy.Publisher('/processed_depth_images_array', Float32MultiArray, queue_size=10)
+        self.pub_image = rospy.Publisher('/processed_depth_images', msg_Image, queue_size=10)
         self.bridge = CvBridge()
         self.sub = rospy.Subscriber(depth_image_topic, msg_Image, self.imageDepthCallback)
 
@@ -25,9 +27,9 @@ class ImageListener:
             cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
             
             cv_image_resized = np.ones([640, 640]).astype(np.uint16)
-            cv_image_resized = cv_image_resized * 10000;  #cv_image[cv_image > 0].max()
+            cv_image_resized = cv_image_resized * 10000  #cv_image[cv_image > 0].max()
 
-            cv_image_resized[80:560,:] = cv_image
+            cv_image_resized[80:560, :] = cv_image
 
             # cv2.imshow('depth', cv_image_resized)
             # cv2.waitKey(0)
@@ -39,10 +41,16 @@ class ImageListener:
             blur_type = 'gaussian'
             final_depths = depth_map_utils.fill_in_fast(cv_image_resized, extrapolate=extrapolate, blur_type=blur_type)
 
+            final_depths = self.bridge.resize(final_depths, (64, 64))
+
             cv_image_msg = msg_Image()
             cv_image_msg = self.bridge.cv2_to_imgmsg(final_depths, encoding="16UC1")
+
+            image_array = Float32MultiArray()
+            image_array.data = list(final_depths.reshape(4096))
         
-            self.pub.publish(cv_image_msg)
+            self.pub_array_image.publish(image_array)
+            self.pub_image.publish(cv_image_msg)
 
 
         except CvBridgeError as e:
